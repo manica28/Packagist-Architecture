d2a52df878672cdf64bdd14d9403d468c55597a6
Nianga Mbaye <mbayenianga28@gmail.com>
2025-07-23 12:14:47 +0000

    changement du nom du projet

--- src/repository/ICitoyenRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Repository;
-interface ICitoyenRepository{
-    public function selectByCni(string $cni);
-    public function insert($citoyen);
-    public function selectById($id);
-}
